f34af17a6638dd74e7502f2254e06fd648dbba20
huayunh <hhuayun0828@gmail.com>
2020-08-18 16:57:03 -0400

    Doing my favorite thing

--- scripts/mui-icon-builder/create-typings.js
+++ scripts/mui-icon-builder/create-typings.js
@@ -8,33 +8,33 @@ const SRC_DIR = path.resolve(__dirname, '../../icons');
 const TARGET_DIR = path.resolve(__dirname, '../../icons');
 
 function normalizeFileName(file) {
-  return path.parse(file).name;
+    return path.parse(file).name;
 }
 
 function createIconTyping(file) {
-  const name = normalizeFileName(file);
-  const contents = `export { default } from '@material-ui/core/SvgIcon';`;
-  return fse.writeFile(path.resolve(TARGET_DIR, `${name}.d.ts`), contents, 'utf8');
+    const name = normalizeFileName(file);
+    const contents = `export { default } from '@material-ui/core/SvgIcon';`;
+    return fse.writeFile(path.resolve(TARGET_DIR, `${name}.d.ts`), contents, 'utf8');
 }
 
 function createIndexTyping(files) {
-  const contents = `
+    const contents = `
 import SvgIcon from '@material-ui/core/SvgIcon';
 type SvgIconComponent = typeof SvgIcon;
-${files.map(file => `export const ${normalizeFileName(file)}: SvgIconComponent;`).join('\n')}
+${files.map((file) => `export const ${normalizeFileName(file)}: SvgIconComponent;`).join('\n')}
 `;
 
-  return fse.writeFile(path.resolve(TARGET_DIR, 'index.d.ts'), contents, 'utf8');
+    return fse.writeFile(path.resolve(TARGET_DIR, 'index.d.ts'), contents, 'utf8');
 }
 
 // Generate TypeScript.
 async function run() {
-  await fse.ensureDir(TARGET_DIR);
-  console.log(`\u{1f52c}  Searching for modules inside "${chalk.dim(SRC_DIR)}".`);
-  const files = glob.sync('!(index)*.js', { cwd: SRC_DIR });
-  const typings = files.map(file => createIconTyping(file));
-  await Promise.all([...typings, createIndexTyping(files)]);
-  console.log(`\u{1F5C4}  Written typings to ${chalk.dim(TARGET_DIR)}.`);
+    await fse.ensureDir(TARGET_DIR);
+    console.log(`\u{1f52c}  Searching for modules inside "${chalk.dim(SRC_DIR)}".`);
+    const files = glob.sync('!(index)*.js', { cwd: SRC_DIR });
+    const typings = files.map((file) => createIconTyping(file));
+    await Promise.all([...typings, createIndexTyping(files)]);
+    console.log(`\u{1F5C4}  Written typings to ${chalk.dim(TARGET_DIR)}.`);
 }
 
 run();
--- scripts/mui-icon-builder/modules/waterfall/metric.js
+++ scripts/mui-icon-builder/modules/waterfall/metric.js
@@ -5,122 +5,122 @@ import warning from 'warning';
 const times = new Map();
 
 const implementations = {
-  mark: {
-    start: name => {
-      times.set(name, performance.now());
-      performance.mark(`metric_${name}_start`);
+    mark: {
+        start: (name) => {
+            times.set(name, performance.now());
+            performance.mark(`metric_${name}_start`);
+        },
+        end: (name) => {
+            const endMark = `metric_${name}_end`;
+            performance.mark(endMark);
+            const startMark = `metric_${name}_start`;
+            performance.measure(name, startMark, endMark);
+            const duration = performance.getEntriesByName(name)[0].duration;
+            return duration;
+        },
     },
-    end: name => {
-      const endMark = `metric_${name}_end`;
-      performance.mark(endMark);
-      const startMark = `metric_${name}_start`;
-      performance.measure(name, startMark, endMark);
-      const duration = performance.getEntriesByName(name)[0].duration;
-      return duration;
+    now: {
+        start: (name) => {
+            times.set(name, performance.now());
+        },
+        end: (name) => {
+            const time = times.get(name);
+            const duration = performance.now() - time;
+            return duration;
+        },
     },
-  },
-  now: {
-    start: name => {
-      times.set(name, performance.now());
+    hrtime: {
+        start: (name) => {
+            // https://nodejs.org/api/process.html#process_process_hrtime_time
+            times.set(name, process.hrtime());
+        },
+        end: (name) => {
+            const time = times.get(name);
+            const durations = process.hrtime(time);
+            const duration = durations[0] / 1e3 + durations[1] / 1e6;
+            return duration;
+        },
     },
-    end: name => {
-      const time = times.get(name);
-      const duration = performance.now() - time;
-      return duration;
-    },
-  },
-  hrtime: {
-    start: name => {
-      // https://nodejs.org/api/process.html#process_process_hrtime_time
-      times.set(name, process.hrtime());
-    },
-    end: name => {
-      const time = times.get(name);
-      const durations = process.hrtime(time);
-      const duration = durations[0] / 1e3 + durations[1] / 1e6;
-      return duration;
-    },
-  },
 };
 
 let getImplementationCache;
 
 function getImplementation() {
-  if (getImplementationCache) {
+    if (getImplementationCache) {
+        return getImplementationCache;
+    }
+
+    if (typeof performance !== 'undefined' && performance.mark) {
+        getImplementationCache = implementations.mark;
+    } else if (typeof performance !== 'undefined' && performance.now) {
+        getImplementationCache = implementations.now;
+    } else if (process.hrtime) {
+        getImplementationCache = implementations.hrtime;
+    } else {
+        throw new Error('No performance API available');
+    }
+
     return getImplementationCache;
-  }
-
-  if (typeof performance !== 'undefined' && performance.mark) {
-    getImplementationCache = implementations.mark;
-  } else if (typeof performance !== 'undefined' && performance.now) {
-    getImplementationCache = implementations.now;
-  } else if (process.hrtime) {
-    getImplementationCache = implementations.hrtime;
-  } else {
-    throw new Error('No performance API available');
-  }
-
-  return getImplementationCache;
 }
 
 class Metric {
-  /**
-   * Call to begin a measurement.
-   */
-  static start(name) {
-    warning(!times.get(name), 'Recording already started');
-    getImplementation().start(name);
-  }
-
-  /**
-   * Returns the duration of the timing metric. The unit is milliseconds.
-   * @type {number}
-   */
-  static end(name) {
-    if (!times.get(name)) {
-      throw new Error(`No such name '${name}' for metric`);
+    /**
+     * Call to begin a measurement.
+     */
+    static start(name) {
+        warning(!times.get(name), 'Recording already started');
+        getImplementation().start(name);
     }
 
-    const duration = getImplementation().end(name);
-    times.delete(name);
-    return duration;
-  }
+    /**
+     * Returns the duration of the timing metric. The unit is milliseconds.
+     * @type {number}
+     */
+    static end(name) {
+        if (!times.get(name)) {
+            throw new Error(`No such name '${name}' for metric`);
+        }
 
-  name = '';
-
-  /**
-   * @param {string} name A name for the metric.
-   */
-  constructor(name) {
-    if (!name) {
-      throw new Error('Please provide a metric name');
+        const duration = getImplementation().end(name);
+        times.delete(name);
+        return duration;
     }
 
-    this.name = name;
-  }
+    name = '';
 
-  /**
-   * Call to begin a measurement.
-   */
-  start(name) {
-    if (name) {
-      throw new Error('The name argument is not supported');
+    /**
+     * @param {string} name A name for the metric.
+     */
+    constructor(name) {
+        if (!name) {
+            throw new Error('Please provide a metric name');
+        }
+
+        this.name = name;
     }
 
-    Metric.start(this.name);
-  }
+    /**
+     * Call to begin a measurement.
+     */
+    start(name) {
+        if (name) {
+            throw new Error('The name argument is not supported');
+        }
 
-  /**
-   * Returns the duration of the timing metric. The unit is milliseconds.
-   * @type {number}
-   */
-  end(name) {
-    if (name) {
-      throw new Error('The name argument is not supported');
+        Metric.start(this.name);
     }
 
-    return Metric.end(this.name);
-  }
+    /**
+     * Returns the duration of the timing metric. The unit is milliseconds.
+     * @type {number}
+     */
+    end(name) {
+        if (name) {
+            throw new Error('The name argument is not supported');
+        }
+
+        return Metric.end(this.name);
+    }
 }
 
 export default Metric;
